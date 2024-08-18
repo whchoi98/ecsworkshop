2cbea3482f12b9f4465b9abd45aa24a9893a4c38
Woo Hyung Choi <whchoi98@gmail.com>
2024-08-18 19:27:20 +0900

    Update ecs-vpc-stack.ts

--- ecs-vpc-stack.ts
+++ ecs-vpc-stack.ts
@@ -1,53 +1,54 @@
 import * as cdk from 'aws-cdk-lib';
 import { Stack, StackProps } from 'aws-cdk-lib';
-import * as ec2 from 'aws-cdk-lib/aws-ec2';
-import * as elbv2 from 'aws-cdk-lib/aws-elasticloadbalancingv2';
+import { Vpc, SubnetType, SecurityGroup, NatProvider, IpAddresses } from 'aws-cdk-lib/aws-ec2';
+import { ApplicationLoadBalancer, ApplicationProtocol, ListenerAction, ApplicationTargetGroup, TargetType } from 'aws-cdk-lib/aws-elasticloadbalancingv2';
 import { Construct } from 'constructs';
 
-export class VpcAlbStack extends Stack {
+export class EcsVpcStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // Create a VPC
-    const vpc = new ec2.Vpc(this, 'MyVPC', {
-      cidr: '10.0.0.0/16',
-      maxAzs: 2, // Default is all AZs in the region
+    // VPC 생성
+    const vpc = new Vpc(this, 'MyVpc', {
+      ipAddresses: IpAddresses.cidr('10.0.0.0/16'),
+      maxAzs: 2,
+      natGateways: 2,
       subnetConfiguration: [
         {
           cidrMask: 24,
           name: 'PublicSubnet',
-          subnetType: ec2.SubnetType.PUBLIC,
+          subnetType: SubnetType.PUBLIC,
         },
         {
           cidrMask: 24,
           name: 'PrivateSubnet',
-          subnetType: ec2.SubnetType.PRIVATE_WITH_NAT,
+          subnetType: SubnetType.PRIVATE_WITH_EGRESS,
         },
       ],
     });
 
-    // Security Group for ALB
-    const albSecurityGroup = new ec2.SecurityGroup(this, 'ALBSecurityGroup', {
+    // ALB 보안 그룹 생성
+    const albSecurityGroup = new SecurityGroup(this, 'ALBSecurityGroup', {
       vpc,
-      description: 'Allow HTTP traffic to ALB',
+      description: 'Allow HTTP access on port 80',
       allowAllOutbound: true,
     });
 
-    albSecurityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(80), 'Allow HTTP traffic');
+    albSecurityGroup.addIngressRule(cdk.aws_ec2.Peer.anyIpv4(), cdk.aws_ec2.Port.tcp(80), 'Allow HTTP traffic');
 
-    // Create Application Load Balancer
-    const alb = new elbv2.ApplicationLoadBalancer(this, 'ApplicationLoadBalancer', {
+    // ALB 생성
+    const alb = new ApplicationLoadBalancer(this, 'ApplicationLoadBalancer', {
       vpc,
       internetFacing: true,
       securityGroup: albSecurityGroup,
     });
 
-    // Create Target Group
-    const targetGroup = new elbv2.ApplicationTargetGroup(this, 'TargetGroup', {
+    // Target Group 생성
+    const targetGroup = new ApplicationTargetGroup(this, 'TargetGroup', {
       vpc,
-      protocol: elbv2.ApplicationProtocol.HTTP,
+      protocol: ApplicationProtocol.HTTP,
       port: 80,
-      targetType: elbv2.TargetType.IP,
+      targetType: TargetType.IP,
       healthCheck: {
         path: '/',
         interval: cdk.Duration.seconds(30),
@@ -57,60 +58,21 @@ export class VpcAlbStack extends Stack {
       },
     });
 
-    // Create Listener for ALB
-    alb.addListener('Listener', {
+    // ALB 리스너 생성
+    alb.addListener('ALBListener', {
+      protocol: ApplicationProtocol.HTTP,
       port: 80,
-      open: true,
-      defaultTargetGroups: [targetGroup],
+      defaultAction: ListenerAction.forward([targetGroup]),
     });
 
-    // Security Group for UI
-    const uiSecurityGroup = new ec2.SecurityGroup(this, 'UISecurityGroup', {
+    // UI 용 보안 그룹 생성
+    const uiSecurityGroup = new SecurityGroup(this, 'UISecurityGroup', {
       vpc,
       description: 'Allow TCP traffic on ports 80 and 8080',
       allowAllOutbound: true,
     });
 
-    uiSecurityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(80), 'Allow HTTP traffic');
-    uiSecurityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(8080), 'Allow traffic on port 8080');
-
-    // SSM Endpoint
-    const ssmSecurityGroup = new ec2.SecurityGroup(this, 'SSMSecurityGroup', {
-      vpc,
-      description: 'Allow SSM access to private instances',
-      allowAllOutbound: true,
-    });
-
-    new ec2.InterfaceVpcEndpoint(this, 'SSMEndpoint', {
-      vpc,
-      service: ec2.InterfaceVpcEndpointAwsService.SSM,
-      securityGroups: [ssmSecurityGroup],
-      subnets: {
-        subnetType: ec2.SubnetType.PRIVATE_WITH_NAT,
-      },
-    });
-
-    new ec2.InterfaceVpcEndpoint(this, 'SSMMessagesEndpoint', {
-      vpc,
-      service: ec2.InterfaceVpcEndpointAwsService.SSM_MESSAGES,
-      securityGroups: [ssmSecurityGroup],
-      subnets: {
-        subnetType: ec2.SubnetType.PRIVATE_WITH_NAT,
-      },
-    });
-
-    new ec2.InterfaceVpcEndpoint(this, 'EC2MessagesEndpoint', {
-      vpc,
-      service: ec2.InterfaceVpcEndpointAwsService.EC2_MESSAGES,
-      securityGroups: [ssmSecurityGroup],
-      subnets: {
-        subnetType: ec2.SubnetType.PRIVATE_WITH_NAT,
-      },
-    });
+    uiSecurityGroup.addIngressRule(cdk.aws_ec2.Peer.anyIpv4(), cdk.aws_ec2.Port.tcp(80), 'Allow HTTP traffic');
+    uiSecurityGroup.addIngressRule(cdk.aws_ec2.Peer.anyIpv4(), cdk.aws_ec2.Port.tcp(8080), 'Allow HTTP traffic on port 8080');
   }
 }
-
-const app = new cdk.App();
-new VpcAlbStack(app, 'VpcAlbStack', {
-  env: { region: 'ap-northeast-2' },
-});
